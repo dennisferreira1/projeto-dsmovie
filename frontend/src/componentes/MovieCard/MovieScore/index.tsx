ebbabcb786ae1394081f5d4a952f861b1ee54a1b
dennisferreira1 <dennisferreira1@hotmail.com>
2022-01-13 12:04:05 -0300

    add moviecard

---
+++ frontend/src/componentes/MovieCard/MovieScore/index.tsx
@@ -0,0 +1,16 @@
+import MovieStar from 'componentes/MovieCard/MovieStar';
+import './styles.css';
+
+function MovieScore() {
+    const score = 2;
+    const count = 5;
+    return (
+        <div className="dsmovie-score-container">
+            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+            <MovieStar />
+            <p className="dsmovie-score-count">{count} avaliações</p>
+        </div>
+    );
+}
+
+export default MovieStar;
--- frontend/src/pages/Listagem/index.tsx
+++ frontend/src/pages/Listagem/index.tsx
@@ -1,9 +1,27 @@
+import MovieCard from "componentes/MovieCard";
 import Paginacao from "componentes/Paginacao";
 
 function Listagem() {
     return (
-       <Paginacao/>
-        
+        <>
+            <Paginacao />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
