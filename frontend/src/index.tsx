af5231ef885fc93b0898925a4a173f9fd9de7931
dennisferreira1 <dennisferreira1@hotmail.com>
2022-01-12 22:03:55 -0300

    add bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,9 @@
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
