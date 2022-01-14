ed6d27d956356de210a90f142b869aec96c01375
dennisferreira1 <dennisferreira1@hotmail.com>
2022-01-14 18:24:47 -0300

    add useState, useEffect

--- frontend/src/pages/Listagem/index.tsx
+++ frontend/src/pages/Listagem/index.tsx
@@ -1,9 +1,25 @@
+import axios from "axios";
 import MovieCard from "componentes/MovieCard";
 import Paginacao from "componentes/Paginacao";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
+import { BASE_URL } from "utils/requests";
 
 function Listagem() {
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=2`)
+        .then(response => {
+        const data = response.data as MoviePage;
+        setPageNumber(data.number);
+        console.log(data);
+        });
+    }, []);
+    
     return (
         <>
+            <h1>{pageNumber}</h1>
             <Paginacao />
             <div className="container">
                 <div className="row">
